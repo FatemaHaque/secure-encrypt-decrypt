c0d55fc22f6153105eea0036d736dcff027c768b
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2024-04-27 02:51:58 +0600

    change7

--- app/api/extractData/route.js
+++ app/api/extractData/route.js
@@ -5,8 +5,10 @@ import connect from "@/lib/mongodb";
 import User from "@/lib/models/User";
 
 export async function GET(request) {
+  console.log("you reached here");
+  console.log(process.env.Token_Secret);
+  await connect();
   try {
-    await connect();
     const userID = await getDataFromToken(request);
     const user = await User.findById({ _id: userID }).select("-password");
     return NextResponse.json(

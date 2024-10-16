4e1893d6050ecf062a84cc9f75eea8190d94cea2
FatemaHaque <haque.marwa@gmail.com>
2024-10-16 12:39:08 -0700

    first commit

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Secure Encryption Decryption",
-  description: "Secure Encryption Decryption",
+  title: "project447",
+  description: "project447",
 };
 
 export default function RootLayout({ children }) {
